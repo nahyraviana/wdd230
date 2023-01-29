f2d0664d10f98eed21422900eba0dae9c8813b20
nahyraviana <nahyraviana@gmail.com>
2023-01-29 01:47:30 -0700

    um

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,3 +1,18 @@
+const datefield = document.querySelector(".date");
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+
+
 let year = document.querySelector("#year");
 const currentYear =  new Date().getFullYear();
 year.textContent= currentYear;
