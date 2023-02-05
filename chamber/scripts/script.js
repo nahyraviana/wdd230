7663a5d793d2ac9d7c2f565478c7f633c603b1de
nahyraviana <nahyraviana@gmail.com>
2023-02-05 10:07:09 -0700

    js

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,7 +1,7 @@
 const datefield = document.querySelector(".date");
 const now = new Date();
 
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+const fulldate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(now);
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
 
