6d12cc551219b55136e031fcc5350953f451ba78
nahyraviana <nahyraviana@gmail.com>
2023-02-19 23:30:07 -0700

    a

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -9,6 +9,15 @@ speed.innerHTML = 4.5;
 speed = parseInt(speed.textContent);
 
 let windChill = document.getElementById("chill");
-windChill =Math.round(35.74 + 0.6215 * temp - 35.75 * (speed ** 0.16) + 0.4275 * temp * (speed ** 0.16));
 
+if (temp >= 50){
+    windChill = N/A;
+}
+else if (speed < 3) {
+     windChill = N/A;
+}
+else{
+windChill =Math.round(35.74 + 0.6215 * temp - 35.75 * (speed ** 0.16) + 0.4275 * temp * (speed ** 0.16));
 chill.innerHTML = windChill;
+}
+
